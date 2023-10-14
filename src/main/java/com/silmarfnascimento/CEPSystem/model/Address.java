c94874217fd35ad6ed156508d50489c624f12a3d
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-14 14:57:58 -0300

    feat: implementa a entidade address e seu repositório

---
+++ src/main/java/com/silmarfnascimento/CEPSystem/model/Address.java
@@ -0,0 +1,22 @@
+package com.silmarfnascimento.CEPSystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Address {
+  @Id
+  private String cep;
+  private String logradouro;
+  private String complemento;
+  private String bairro;
+  private String localidade;
+  private String uf;
+  private String ibge;
+  private String gia;
+  private String ddd;
+  private String siafi;
+}
---
+++ src/main/java/com/silmarfnascimento/CEPSystem/model/IAddressRepository.java
@@ -0,0 +1,6 @@
+package com.silmarfnascimento.CEPSystem.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IAddressRepository extends JpaRepository<Address, String> {
+}

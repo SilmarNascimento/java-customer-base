f912b20ee73acfb81e3716aa6005b53d85299500
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-14 14:48:31 -0300

    inicializa o projeto de sistema de CEP

---
+++ src/main/java/com/silmarfnascimento/CEPSystem/CepSystemApplication.java
@@ -0,0 +1,13 @@
+package com.silmarfnascimento.CEPSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silmarfnascimento/CEPSystem/CepSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silmarfnascimento.CEPSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

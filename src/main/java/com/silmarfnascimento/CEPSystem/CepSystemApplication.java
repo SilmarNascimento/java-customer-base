51f152476dbfd100e435273ad5fce9ab46148ff3
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-14 21:26:09 -0300

    fix: habilita o FeingClients na aplicação

--- src/main/java/com/silmarfnascimento/CEPSystem/CepSystemApplication.java
+++ src/main/java/com/silmarfnascimento/CEPSystem/CepSystemApplication.java
@@ -2,8 +2,10 @@ package com.silmarfnascimento.CEPSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CepSystemApplication {
 
 	public static void main(String[] args) {
